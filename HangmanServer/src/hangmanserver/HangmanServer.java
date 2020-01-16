34428e394629d7c55c78f641e6205dcb055b2e50
Dovydas <dovydas.speckauskas@ktu.edu>
2020-01-16 17:51:17 +0200

    Server
    Added communication realization to the server.

--- HangmanServer/src/hangmanserver/HangmanServer.java
+++ HangmanServer/src/hangmanserver/HangmanServer.java
@@ -1,21 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hangmanserver;
 
-/**
- *
- * @author Dovydas
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HangmanServer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private final static int PORT_NO = 4000;
+    private final ServerSocket listener;
+    private final List<Connection> clients;
+
+    HangmanServer() throws IOException {
+        listener = new ServerSocket(PORT_NO);
+        clients = new ArrayList<>();
+        System.out.println("Listening on port: " + PORT_NO);
     }
-    
+
+    void runServer() {
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                System.out.println("Accepted new connection");
+                Connection con = new Connection(socket);
+                clients.add(con);
+                con.start();
+            }
+        } catch (IOException ioe) {
+            System.err.println("I/O error: " + ioe.getMessage());
+        }
+    }
+
+    private class Connection extends Thread {
+
+        private BufferedReader br;
+        private PrintWriter pw;
+
+        Connection(Socket s) throws IOException {
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            pw = new PrintWriter(s.getOutputStream(), true);
+        }
+
+        @Override
+        public void run() {
+            send("Welcome!");
+            String line;
+            try {
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                    //send(line);
+                }
+            } catch (IOException ioe) {
+                System.err.println("I/O error: " + ioe.getMessage());
+            } finally {
+                System.out.println("Client removed");
+                clients.remove(this);
+            }
+        }
+
+        private void send(String message) {
+            pw.println(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Server starting.");
+            new HangmanServer().runServer();
+        } catch (IOException ioe) {
+            System.err.println("Unable to create server socket. " + ioe.getMessage());
+        }
+    }
 }
